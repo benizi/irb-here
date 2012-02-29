b7b8b6a2a316f57fada6c73f0f86346ac78707e3
Benjamin R. Haskell <ruby@benizi.com>
2012-02-28 19:02:04 -0500

    Minimal functionality

--- lib/irb-here.rb
+++ lib/irb-here.rb
@@ -1,7 +1,32 @@
 require "irb-here/version"
+require 'irb'
 
-module Irb
-  module Here
-    # Your code goes here...
+#
+# Extend IRB with a ##start_with_binding method
+#
+module IRB
+  def IRB.start_with_binding(binding)
+    IRB.setup(__FILE__)
+    irb = Irb.new(WorkSpace.new(binding))
+    @CONF[:MAIN_CONTEXT] = irb.context
+    irb.eval_input
+  end
+end
+
+#
+# Extend Binding with an #irb method
+#
+class Binding
+  def irb
+    IRB.start_with_binding(self)
+  end
+end
+
+#
+# Extend Kernel with a #breakpoint method
+#
+module Kernel
+  def breakpoint(binding = nil)
+    IRB.start_with_binding(binding)
   end
 end
